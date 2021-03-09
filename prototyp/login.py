11b1fd245cf7296dff3e6c515df5f6f8de2d68da
Florian Schmitz <florian@ruediger-schmitz.de>
2021-03-09 19:39:32 +0100

    driver

--- prototyp/login.py
+++ prototyp/login.py
@@ -7,23 +7,24 @@ url = "https://www.leitstellenspiel.de/users/sign_in"
 user = ""
 pw = ""
 
-browser = webdriver.Safari()
+driver = webdriver.Safari()
+driver.maximize_window()
 
 
 def start():
 
-    browser.get(url)
+    driver.get(url)
 
-    username = browser.find_element_by_id("user_email")
-    password = browser.find_element_by_id("user_password")
+    username = driver.find_element_by_id("user_email")
+    password = driver.find_element_by_id("user_password")
 
     username.send_keys(user)
     password.send_keys(pw)
 
-    browser.find_element_by_name("commit").click()
+    driver.find_element_by_name("commit").click()
 
     sleep(2)
-    browser.find_element_by_class_name("cookies-eu-ok").click()
+    driver.find_element_by_class_name("cookies-eu-ok").click()
 
 def stop():
-    browser.quit()
+    driver.quit()
