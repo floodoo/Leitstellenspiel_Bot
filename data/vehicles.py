927b5ba4711db4cbdbf84a11348ea11d5b477557
Florian Schmitz <florian@ruediger-schmitz.de>
2021-03-10 18:42:19 +0100

    update vehicles.py

--- data/vehicles.py
+++ data/vehicles.py
@@ -9,6 +9,7 @@ class Vehicles():
     
     def __init__(self, driver):
         self.driver = driver
+        self.filename = "data.json"
    
     # The simple version did not work
     def get_vehicle_api(self):
@@ -18,32 +19,32 @@ class Vehicles():
         json_data = self.driver.find_element_by_xpath("/html/body/pre")
         json_data = json_data.text
         
-        with open("data.json", "w") as json_file:
+        with open(self.filename, "w") as json_file:
             json.dump(json_data, json_file)
             json_file.close()
             
         # Remove Backslash and "
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             filedata = json_file.read()
             filedata = filedata.replace("\\", "")
             filedata = filedata[1:-1]
             json_file.close()
 
-        with open("data.json", "w") as json_file:
+        with open(self.filename, "w") as json_file:
             json_file.write(filedata)
             json_file.close()
         
         # Beautify Json File
-        with open('data.json') as json_file:
+        with open(self.filename) as json_file:
             obj = json.load(json_file)
-            outfile = open('data.json', "w")
+            outfile = open(self.filename, "w")
             outfile.write(json.dumps(obj, indent=4, sort_keys=True))
             outfile.close()
         
     def get_all_vehicle_id(self):
         all_vehicle_id = []
 
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             data = json.load(json_file)
             for i in data:
                 all_vehicle_id.append(i["id"])
@@ -55,7 +56,7 @@ class Vehicles():
     def get_all_vehicle_types(self):
         all_vehicle_types = []
 
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             data = json.load(json_file)
             for i in data:
                 all_vehicle_types.append(i["vehicle_type"])
@@ -67,7 +68,7 @@ class Vehicles():
     def get_all_vehicle_fms_real(self):
         all_vehicle_fms_real = []
 
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             data = json.load(json_file)
             for i in data:
                 all_vehicle_fms_real.append(i["fms_real"])
@@ -79,7 +80,7 @@ class Vehicles():
     def get_all_vehicle_fms_show(self):
         all_vehicle_fms_show = []
 
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             data = json.load(json_file)
             for i in data:
                 all_vehicle_fms_show.append(i["fms_show"])
@@ -91,7 +92,7 @@ class Vehicles():
     def get_all_vehicle_target_type(self):
         all_vehicle_target_type = []
 
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             data = json.load(json_file)
             for i in data:
                 all_vehicle_target_type.append(i["target_type"])
@@ -103,7 +104,7 @@ class Vehicles():
     def get_all_vehicle_target_id(self):
         all_vehicle_target_id = []
 
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             data = json.load(json_file)
             for i in data:
                 all_vehicle_target_id.append(i["target_id"])
@@ -115,7 +116,7 @@ class Vehicles():
     def get_all_vehicle_building_id(self):
         all_vehicle_building_id = []
 
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             data = json.load(json_file)
             for i in data:
                 all_vehicle_building_id.append(i["building_id"])
@@ -127,7 +128,7 @@ class Vehicles():
     def get_all_vehicle_caption(self):
         all_vehicle_caption = []
 
-        with open("data.json", "r") as json_file:
+        with open(self.filename, "r") as json_file:
             data = json.load(json_file)
             for i in data:
                 all_vehicle_caption.append(i["caption"])
