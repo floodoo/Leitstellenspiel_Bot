1d08c0d0106c3f32eb301a1aaa5c245a77d6c0ab
Florian Schmitz <florian@ruediger-schmitz.de>
2021-03-10 13:40:49 +0100

    get Vehicle Api

--- data/vehicles.py
+++ data/vehicles.py
@@ -2,6 +2,8 @@ import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from time import sleep
+import json
+import simplejson
 
 
 class Vehicles():
@@ -9,34 +11,61 @@ class Vehicles():
     def __init__(self, driver):
         self.driver = driver
 
-    def get_all_vehicle_id(self):
-        url = "https://www.leitstellenspiel.de/vehicles/"
-        all_vehicles = []
+    # def get_all_vehicle_id(self):
+    #     url = "https://www.leitstellenspiel.de/vehicles/"
+    #     all_vehicle_id = []
 
-        driver.get(url)
-        vehicles = self.driver.find_elements_by_xpath(
-            "//a[contains(@href,'vehicles')]")
+    #     self.driver.get(url)
+    #     vehicles = self.driver.find_elements_by_xpath(
+    #         "//a[contains(@href,'vehicles')]")
         
-        for i in vehicles:
-            all_vehicles.append(i.get_attribute("href")[41:])
+    #     for i in vehicles:
+    #         all_vehicle_id.append(i.get_attribute("href")[41:])
         
-        return all_vehicles
+    #     return all_vehicle_id
 
-    def get_all_vehicle_types(self):
-        url = "https://www.leitstellenspiel.de/vehicles/"
-        all_vehicle_types = []
+    # def get_all_vehicle_types(self):
+    #     url = "https://www.leitstellenspiel.de/vehicles/"
+    #     all_vehicle_types = []
 
-        driver.get(url)
-        vehicles = self.driver.find_elements_by_class_name(
-            "vehicle_image_reload")
+    #     self.driver.get(url)
+    #     vehicles = self.driver.find_elements_by_class_name(
+    #         "vehicle_image_reload")
         
-        for i in vehicles:
-            all_vehicle_types.append(i.get_attribute("vehicle_type_id"))
+    #     for i in vehicles:
+    #         all_vehicle_types.append(i.get_attribute("vehicle_type_id"))
             
-        return all_vehicle_types
+    #     return all_vehicle_types
    
-   
-   
+    def get_json(self):
+        url = "https://www.leitstellenspiel.de/api/vehicles"
+       
+        self.driver.get(url)
+        json_data = self.driver.find_element_by_xpath("/html/body/pre")
+        json_data = json_data.text
+        
+        with open("data.json", "w") as json_file:
+            json.dump(json_data, json_file)
+            json_file.close()
+            
+        with open("data.json", "r") as json_file:
+            filedata = json_file.read()
+            filedata = filedata.replace("\\", "")
+            filedata = filedata[1:-1]
+            json_file.close()
+
+        with open("data.json", "w") as json_file:
+            json_file.write(filedata)
+            json_file.close()
+            
+        with open('data.json') as json_file:
+            obj = json.load(json_file)
+            outfile = open('data.json', "w")
+            outfile.write(json.dumps(obj, indent=4, sort_keys=True))
+            outfile.close()
+        
+        driver.close()
+        
         
 url = "https://www.leitstellenspiel.de/users/sign_in"
 user = ""
@@ -59,5 +88,4 @@ sleep(2)
 driver.find_element_by_class_name("cookies-eu-ok").click()
 
 test = Vehicles(driver)
-print(test.get_all_vehicle_id())
-print(test.get_all_vehicle_types())
+test.get_json()
