ae6a2bfa6383d66a64dcff239b1682ee00ac8383
Florian Schmitz <florian@ruediger-schmitz.de>
2021-03-14 23:35:43 +0100

    Get required vehicles for all emergencies

--- control.py
+++ control.py
@@ -15,6 +15,7 @@ class Control():
 
         user = ""
         pw = ""
+        self.emergency_list = []
 
         self.driver = webdriver.Safari()
         self.driver.get(url)
@@ -29,14 +30,28 @@ class Control():
 
         sleep(uniform(2, 3))
         self.driver.find_element_by_class_name("cookies-eu-ok").click()
+        
+        self.emergencies = Emergencies(self.driver)
+        self.vehicle = Vehicles(self.driver)
 
-        thread = threading.Thread(target=self.update_vehicle_data)
-        thread.start()
+        threading.Thread(target=self.update_vehicle_data).start()
+        threading.Thread(target=self.update_active_emergency_list).start()
+        self.go_through_emergencies()
 
     def update_vehicle_data(self):
         while True:
-            vehicle = Vehicles(self.driver)
-            vehicle.get_vehicle_api()
+            self.vehicle.get_vehicle_api()
             sleep(uniform(10, 20))
+            
+    def update_active_emergency_list(self):
+        while True:
+            self.emergency_list = self.emergencies.get_all_active_Emergencies_id()
+            sleep(uniform(50, 80))
+            
+    def go_through_emergencies(self):
+        
+        for emergency in self.emergency_list:
+            self.emergencies.get_required_vehicles(emergency)
+            sleep(uniform(5, 10))
 
 Control()
