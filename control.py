eaf3860b44579d7a4431549e8a7d8a666bbaeb16
Florian Schmitz <florian@ruediger-schmitz.de>
2021-03-22 12:41:03 +0100

    dauerhaftes farmen

--- control.py
+++ control.py
@@ -25,7 +25,8 @@ class Control():
         self.emergency_list = []
         self.use_driver = False
         self.load = True
-        # self.benoetigte_Fahrzeuge = []
+        self.benoetigte_Fahrzeuge = [
+            "Benötigte Löschfahrzeuge", "Benötigte Streifenwagen"]
         self.LF_list = [0, 1, 6, 7, 8, 9, 30, 37]
 
         self.driver = webdriver.Safari()
@@ -49,9 +50,7 @@ class Control():
         self.use_driver = False
 
         self.update_vehicle_data()
-        threading.Thread(target=self.update_active_emergency_list).start()
-        self.update_active_emergency_list()
-        self.go_through_emergencies()
+        threading.Thread(target=update_active_emergency_list).start()
 
     def update_vehicle_data(self):
         if self.use_driver != True:
@@ -59,20 +58,26 @@ class Control():
             logging.info("Update Vehicles")
             self.vehicle.get_vehicle_api()
             self.use_driver = False
+            self.update_active_emergency_list()
+            sleep(uniform())
 
     def update_active_emergency_list(self):
-        if self.use_driver != True:
-            self.use_driver = True
-            self.load = True
-            logging.info("Update active emergency list")
-            self.emergency_list = self.emergencies.get_all_active_Emergencies_id()
-            logging.info("Active Emergencies: " + str(self.emergency_list))
-            self.use_driver = False
-            self.load = False
+        while True:
+            if self.use_driver != True:
+                self.use_driver = True
+                self.load = True
+                self.emergency_list = []
+                logging.info("Update active emergency list")
+                self.emergency_list = self.emergencies.get_all_active_Emergencies_id()
+                logging.info("Active Emergencies: " + str(self.emergency_list))
+                self.use_driver = False
+                self.load = False
+                self.go_through_emergencies()
+                sleep(uniform(120, 180))
 
     def go_through_emergencies(self):
-
-        while True:
+        end = False
+        while end != True:
 
             if self.load == False:
 
@@ -88,61 +93,65 @@ class Control():
 
                     self.use_driver = False
                     self.send_required_vehicles()
-                    sleep(uniform(120, 180))
+                    end = True
 
     def send_required_vehicles(self):
-        if self.load == False and self.use_driver == False:
-            for mission_id in self.emergency_list:
+        end = False
+        while end != True:
+            if self.load == False and self.use_driver == False:
+                for mission_id in self.emergency_list:
 
-                logging.info("Current mission ID: " + mission_id)
-                with open('required_vehicles_' + mission_id + '.json', 'r') as json_file:
-                    obj = json.loads(json_file.read())
+                    logging.info("Current mission ID: " + mission_id)
+                    with open('required_vehicles_' + mission_id + '.json', 'r') as json_file:
+                        obj = json.loads(json_file.read())
 
-                with open('vehicle_data.json', 'r') as vehicle_data_file:
-                    vehicle_data = json.loads(
-                        vehicle_data_file.read())
+                    with open('vehicle_data.json', 'r') as vehicle_data_file:
+                        vehicle_data = json.loads(
+                            vehicle_data_file.read())
 
-                vehicle_list = []
-                
-                if "Benötigte Löschfahrzeuge" in obj:
+                    vehicle_list = []
 
-                    vehicle_number_needet = int(
-                        obj["Benötigte Löschfahrzeuge"])
+                    for benoetigt in self.benoetigte_Fahrzeuge:
 
-                    counter = 1
+                        if benoetigt in obj:
+                            vehicle_number_needet = int(
+                                obj[benoetigt])
 
-                    for i in vehicle_data:
-                        if i["vehicle_type"] in self.LF_list and i["fms_show"] == 2 and i["fms_real"] == 2 and counter <= vehicle_number_needet or i["vehicle_type"] in self.LF_list and i["fms_show"] == 1 and i["fms_real"] == 1 and counter <= vehicle_number_needet:
-                            counter += 1
-                            vehicle_id = i["id"]
-                            vehicle_list.append(vehicle_id)
-                            
-                elif "Benötigte Streifenwagen" in obj:
-                    
-                    vehicle_number_needet = int(
-                        obj["Benötigte Streifenwagen"])
+                            counter = 1
 
-                    counter = 1
+                            if benoetigt == "Benötigte Löschfahrzeuge":
 
-                    for i in vehicle_data:
-                        if i["vehicle_type"] == 32 and i["fms_show"] == 2 and i["fms_real"] == 2 and counter <= vehicle_number_needet or i["vehicle_type"] == 32 and i["fms_show"] == 1 and i["fms_real"] == 1 and counter <= vehicle_number_needet:
-                            counter += 1
-                            vehicle_id = i["id"]
-                            vehicle_list.append(vehicle_id)
+                                for i in vehicle_data:
+                                    if i["vehicle_type"] in self.LF_list and i["fms_show"] == 2 and i["fms_real"] == 2 and counter <= vehicle_number_needet or i["vehicle_type"] in self.LF_list and i["fms_show"] == 1 and i["fms_real"] == 1 and counter <= vehicle_number_needet:
+                                        counter += 1
+                                        vehicle_id = i["id"]
+                                        vehicle_list.append(vehicle_id)
+
+                            if benoetigt == "Benötigte Streifenwagen":
+
+                                for i in vehicle_data:
+                                    if i["vehicle_type"] == 32 and i["fms_show"] == 2 and i["fms_real"] == 2 and counter <= vehicle_number_needet or i["vehicle_type"] == 32 and i["fms_show"] == 1 and i["fms_real"] == 1 and counter <= vehicle_number_needet:
+                                        counter += 1
+                                        vehicle_id = i["id"]
+                                        vehicle_list.append(vehicle_id)
 
                     logging.info("Send vehicles: " + str(vehicle_list))
                     self.emergencies.send_required_vehicles(
                         mission_id, vehicle_list)
 
-                sleep(10)
-                logging.debug(
-                    "Delete file: required_vehicles_" + mission_id + ".json")
-                os.remove("required_vehicles_" + mission_id + ".json")
-                logging.debug("Delete file: vehicle_data.json")
-                os.remove("vehicle_data.json")
-                logging.debug("Rewrite file: vehicle_data.json")
-                self.update_vehicle_data()
-                sleep(uniform(10, 20))
+                    sleep(10)
+                    logging.debug(
+                        "Delete file: required_vehicles_" + mission_id + ".json")
+                    os.remove("required_vehicles_" + mission_id + ".json")
+                    logging.debug("Delete file: vehicle_data.json")
+                    os.remove("vehicle_data.json")
+                    logging.debug("Rewrite file: vehicle_data.json")
+                    self.update_vehicle_data()
+                    sleep(uniform(10, 20))
+
+                print("Done.....")
+
+            end = True
 
 
 Control()
